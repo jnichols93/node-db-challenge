02d3153427588e06c29b9761d65670df091bbea2
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-11-08 11:45:52 -0600

    build up to migrate

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,16 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   },
 
   staging: {
